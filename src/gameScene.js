8ed061a064c6f5a18238192f270c3cdf2f8c809c
eburgstahler <eburgstahler@gmail.com>
2026-03-01 11:10:38 -0500

    Create gameScene.js

---
+++ src/gameScene.js
@@ -0,0 +1,25 @@
+class GameScene extends Phaser.Scene {
+	constructor() {
+		super("GameScene")
+	}
+ preload() {
+    this.load.spritesheet("player", "duckPlayer.png", {
+      frameWidth: 20,
+      frameHeight: 20,
+    });
+
+    this.load.image("tileset", "assets/tileset.png");
+    // created with Tiled tilemap editor
+    this.load.tilemapTiledJSON("map", "assets/map.json");
+
+    this.load.image("coin", "rubberDuck.png");
+    this.load.image("enemy", "assets/enemy.png");
+
+    this.load.audio("jump", ["assets/jump.ogg", "assets/jump.mp3"]);
+    this.load.audio("coin", ["rubberDuck.ogg", "rubberDuck.mp3"]);
+    this.load.audio("dead", ["assets/dead.ogg", "assets/dead.mp3"]);
+
+    this.load.image("pixel", "assets/pixel.png");
+  }
+
+export default GameScene;
